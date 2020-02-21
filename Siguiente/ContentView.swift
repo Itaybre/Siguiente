885fad84e0a74a5699f83f5961cbe3ba80444926
Itay <itay@itaysoft.com>
2020-02-20 23:39:42 -0300

    Initial Commit

---
+++ Siguiente/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  Siguiente
+//
+//  Created by Itay Brenner on 2/20/20.
+//  Copyright © 2020 Itay Brenner. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
